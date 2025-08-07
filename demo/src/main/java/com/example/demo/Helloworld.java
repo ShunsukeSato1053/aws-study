db1e5cfbfffa1c9a0673b5e50e8e2b4e9d36db27
Shunsuke Sato <satoa1053@gmail.com>
2025-08-07 13:02:25 +0900

    feat: warファイルに修正

--- demo/src/main/java/com/example/demo/Helloworld.java
+++ demo/src/main/java/com/example/demo/Helloworld.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class Helloworld {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Helloworld.class, args);
-	}
 
 	@GetMapping("/")
 	public String hello() {
